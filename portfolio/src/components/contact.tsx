fd749351a19dbb59e1b92ebeb738cd014c7e8e5b
Agustin F. Ruiz <62730262+darksoul96@users.noreply.github.com>
2024-03-12 01:26:53 -0300

    Added social icons

--- portfolio/src/components/contact.tsx
+++ portfolio/src/components/contact.tsx
@@ -1,9 +1,21 @@
 import "./contact.css"
+import { SocialIcon } from 'react-social-icons'
+import whatsapp from '../assets/svg-files/whatsapp.png';
 
 export default function Contact() {
     return (
         <section className="contact">
-            <h1>Contact Section</h1>
+            <h1>Connect with me</h1>
+
+            <div id="socials">
+                <SocialIcon url="https://twitter.com/agustin_ruiz96" className="socialIcon"/>
+                <SocialIcon url="https://github.com/darksoul96" className="socialIcon"/>
+                <SocialIcon url="https://www.linkedin.com/in/agustin-federico-ruiz-991904220/" className="socialIcon"/>
+                <img src={whatsapp} alt="" id="whatsapp-logo" className="socialIcon"/>
+                <SocialIcon url="mailto:agustinruiz96@gmail.com?Subject=Hello%20world!" className="socialIcon"></SocialIcon>
+                
+            </div>
+
         </section>
     )
 }
--- portfolio/src/components/technologies.tsx
+++ portfolio/src/components/technologies.tsx
@@ -9,45 +9,61 @@ import springLogo from '../assets/svg-files/springio-icon.svg';
 import nodejsLogo from '../assets/svg-files/nodejs-icon.svg';
 import flutterLogo from '../assets/svg-files/flutterio-icon.svg';
 import { useRef, useState } from "react";
+import Slider from "react-slick";
+
 
 export default function Technologies() {
 
     const angular = useRef<HTMLDivElement>(null);
-
+    var settings = {
+        dots: false,
+        infinite: true,
+        speed: 2500,
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        autoplay: true,
+        autoplaySpeed: 500,
+        cssEase: "linear",
+        pauseOnHover: true,
+        nextArrow: <></>,
+        prevArrow: <></>
+      };
     return (
         <section className="technologies">
             <h1>Technologies</h1>
-
-            <div className="technologies-carousel">
-                <div className="carousel-element" ref={angular}>
-                    <img src={angularLogo} alt="" className="logo-element"/>
-                </div>
-                <div className="carousel-element">
-                    <img src={javaLogo} alt="" className="logo-element"/>
-                </div>
-                <div className="carousel-element">
-                    <img src={javascriptLogo} alt="" className="logo-element"/>
-                </div>
-                <div className="carousel-element">
-                    <img src={reactLogo} alt="" className="logo-element"/>
-                </div>
-                <div className="carousel-element">
-                    <img src={cppLogo} alt="" className="logo-element"/>
-                </div>
-                <div className="carousel-element">
-                    <img src={typescriptLogo} alt="" className="logo-element"/>
-                </div>
-                <div className="carousel-element">
-                    <img src={springLogo} alt="" className="logo-element"/>
-                </div>
-                <div className="carousel-element">
-                    <img src={nodejsLogo} alt="" className="logo-element"/>
-                </div>
-                <div className="carousel-element">
-                    <img src={flutterLogo} alt="" className="logo-element"/>
-                </div>
-                
+            <div className="slider-container">
+                <Slider {...settings}>
+                    <div className="carousel-element" ref={angular}>
+                        <img src={angularLogo} alt="" className="logo-element"/>
+                    </div>
+                    <div className="carousel-element">
+                        <img src={javaLogo} alt="" className="logo-element"/>
+                    </div>
+                    <div className="carousel-element">
+                        <img src={javascriptLogo} alt="" className="logo-element"/>
+                    </div>
+                    <div className="carousel-element">
+                        <img src={reactLogo} alt="" className="logo-element"/>
+                    </div>
+                    <div className="carousel-element">
+                        <img src={cppLogo} alt="" className="logo-element"/>
+                    </div>
+                    <div className="carousel-element">
+                        <img src={typescriptLogo} alt="" className="logo-element"/>
+                    </div>
+                    <div className="carousel-element">
+                        <img src={springLogo} alt="" className="logo-element"/>
+                    </div>
+                    <div className="carousel-element">
+                        <img src={nodejsLogo} alt="" className="logo-element"/>
+                    </div>
+                    <div className="carousel-element">
+                        <img src={flutterLogo} alt="" className="logo-element"/>
+                    </div>
+                    
+                </Slider>
             </div>
+            
         </section>
     )
 }
